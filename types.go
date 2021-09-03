0977f992c3ca912758bb6ba55ca8cd7c35f4e79a
d.soldatenko <d.soldatenko@rubetek.com>
2021-09-03 14:39:37 +0300

    some work

---
+++ types.go
@@ -0,0 +1,24 @@
+package gst
+
+///*
+//#cgo pkg-config: gstreamer-1.0 gstreamer-app-1.0
+//#include "gst.h"
+//#include "gstenumtypes.h"
+//#include "gstformat.h"
+//*/
+
+import "C"
+
+
+const (
+	Format_time  = 3
+)
+
+//const (
+//	Format_undefined Format = C.GST_FORMAT_UNDEFINED
+//	Format_default   Format = C.GST_FORMAT_DEFAULT
+//	Format_bytes     Format = C.GST_FORMAT_BYTES
+//	Format_time      Format = C.GST_FORMAT_TIME
+//	Format_buffers   Format = C.GST_FORMAT_BUFFERS
+//	Format_percent   Format = C.GST_FORMAT_PERCENT
+//)
